dc11d08299eb3dae58bdb7150ac4d37b80d86673
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2023-12-11 16:28:44 +0530

    Ok

---
+++ Python-Tutorial/1.py
@@ -0,0 +1,27 @@
+# name = "Kajal"
+# age= 21
+
+# name="Sonam"
+# age= 22.0
+
+# is_adult = True
+
+# print(name)
+# print(age)
+
+
+# Add a person with name Tony Stark.
+# Tony's age is 51 years old.
+# Tony is a genius.
+
+# name = input("What is your name? ")
+# print("Hello "+ name)
+
+# Tony is secretly a superhero. Ask him for his superhero name& show it on the terminal.
+
+# old_age = input("Enter your old age : ")
+# new_age = int(old_age) + 2
+# print(new_age)
+
+number = 18
+print(float(18))
---
+++ Python-Tutorial/2.py
@@ -0,0 +1,7 @@
+#  Sum of two number
+first = input("Enter first number : ")
+second = input("Enter second number : ")
+
+sum = int(first )+ int(second)
+
+print(sum)

74792c132d697c3a7a52064f7b2a131b79babed6
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-03-20 22:21:36 +0530

    Python-Practice

---
+++ Day-31/index.py
@@ -0,0 +1,12 @@
+s = {2,4,2,6}
+print(s)
+
+info = {"Carla", 19, False, 5.9, 19}
+print(info)
+
+
+kajal = set()
+print(type(kajal))
+
+for value in info:
+  print(value)
--- Python-Project/Track-phone-num-location/main.py
+++ Python-Project/Track-phone-num-location/main.py
@@ -1,4 +1,7 @@
 import phonenumbers
+# import opencage
+# import folium
+# from myphone import number
 from test import number
 
 
@@ -10,3 +13,23 @@ print(geocoder.description_for_number(ch_number, "en"))
 from phonenumbers import carrier
 service_number = phonenumbers.parse(number, "RO")
 print(carrier.name_for_number(service_number, "en"))
+
+
+# from opencage.geocoder import OpenCageGeocode
+
+# key = '706f1a53e1514387a1bfce07e234faf3'
+
+# geocoder = OpenCageGeocode(key)
+# query = str(location)
+# results = geocoder.geocode(query)
+# # print(results)
+
+# lat = results[0]['geometry']['lat']
+# lng = results[0]['geometry']['lng']
+
+# print(lat, lng)
+
+# myMap = folium.Map(location = [lat, lng], zoom_start = 9)
+# folium.Marker([lat, lng], popup=location).add_to(myMap)
+
+# myMap.save("mylocation.html")
